521a5ac21e8f484c56c2fbc11f7813fee0077eb7
João Hudson <joao.hudson@taking.com.br>
2023-10-27 12:07:49 -0300

    ajustando adição de imagens

--- client/index.js
+++ client/index.js
@@ -2,23 +2,66 @@ const addButton = document.getElementById('addButton')
 const addLinkDialog = document.getElementById('addLinkDialog')
 const addLinkField = document.getElementById('addLinkField')
 const addLinkButton = document.getElementById('addLinkButton')
-const images = document.getElementById('images')
+const imagesDiv = document.getElementById('images')
+const cancelLinkButton = document.getElementById('cancelLinkButton')
 
-function addLink(link) {
+let data = {
+    images: [] 
+}
+
+if(localStorage.getItem('data') != null) {
+    const dataJson = localStorage.getItem('data')
+    data = JSON.parse(dataJson)
+}
+
+function buildImageDiv(url) {
     const img = document.createElement('img')
-    img.src = link
+    img.src = url
     img.classList.add('galleryImage')
     const imgDiv = document.createElement('div')
     imgDiv.appendChild(img)
-    images.appendChild(imgDiv)
+    return img
+}
+
+function update() {
+    imagesDiv.innerText = ''
+    data.images.forEach(image => {
+        imagesDiv.appendChild(buildImageDiv(image))
+    })
+}
+
+function save() {
+    localStorage.setItem('data', JSON.stringify(data))
+}
+
+function addLink(link) {
+   data.images.push(link)
+   update()
+   save()
+}
+
+function showLinkDialog(show) {
+    if(show) {
+        addButton.hidden = true
+        addLinkDialog.hidden = false
+        addLinkDialog.value = ''
+    } else {
+        addButton.hidden = false
+        addLinkDialog.hidden = true
+    }
 }
 
 addLinkButton.onclick = () => {
     addLink(addLinkField.value)
-    addLinkDialog.hidden = true
-    addLinkField.value = ''
+    showLinkDialog(false)
+}
+
+cancelLinkButton.onclick = () => {
+    showLinkDialog(false)
 }
 
 addButton.onclick = () => {
-    addLinkDialog.hidden = false
+    showLinkDialog(true)
 }
+
+update()
