c9050714d3423b7bf792d8f392aff56bcbd53660
João Hudson <joao.hudson@taking.com.br>
2023-11-07 11:36:27 -0300

    adicionando tratamento para imagem não encontrada

--- client/components/image.js
+++ client/components/image.js
@@ -2,6 +2,10 @@ export function buildImageDiv(url, onClick) {
     const img = document.createElement('img')
     img.src = url
     img.onclick = onClick
+    img.onerror = () => {
+        img.onerror = null
+        img.src = './resource/not-found-image.webp'
+    }
     img.classList.add('galleryImage')
     const imgDiv = document.createElement('div')
     imgDiv.appendChild(img)
