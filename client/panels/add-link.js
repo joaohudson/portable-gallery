523988e6a941b6331e0dbb1074c563988e664d9b
João Hudson <joao.hudson@taking.com.br>
2023-11-08 14:37:52 -0300

    formatting

--- client/panels/add-link.js
+++ client/panels/add-link.js
@@ -3,20 +3,20 @@ const rootPanel = document.getElementById('rootPanel')
 export function showAddLinkPanel(onAdd, onCancel) {
 
     const confirmButton = document.createElement('button')
-        confirmButton.innerText = '>>'
+    confirmButton.innerText = '>>'
     const linkInput = document.createElement('input')
-        linkInput.classList.add('textInput')
-        linkInput.type = 'text'
+    linkInput.classList.add('textInput')
+    linkInput.type = 'text'
     const cancelButton = document.createElement('button')
-        cancelButton.innerText = 'X'
+    cancelButton.innerText = 'X'
     const addLinkContent = document.createElement('div')
-        addLinkContent.classList.add('modalContent')
-        addLinkContent.appendChild(cancelButton)
-        addLinkContent.appendChild(linkInput)
-        addLinkContent.appendChild(confirmButton)
+    addLinkContent.classList.add('modalContent')
+    addLinkContent.appendChild(cancelButton)
+    addLinkContent.appendChild(linkInput)
+    addLinkContent.appendChild(confirmButton)
     const addLinkPanel = document.createElement('div')
-        addLinkPanel.classList.add('modal')
-        addLinkPanel.appendChild(addLinkContent)
+    addLinkPanel.classList.add('modal')
+    addLinkPanel.appendChild(addLinkContent)
     
 
     confirmButton.onclick = () => {
